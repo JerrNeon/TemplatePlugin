979b95c0220b19947f5623cc4a347b2679448158
GitHub Action <action@github.com>
2023-08-30 06:46:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/jerrneon/templateplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/jerrneon/templateplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.jerrneon.templateplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jerrneon.templateplugin.MyBundle
+import com.github.jerrneon.templateplugin.services.MyProjectService
 import javax.swing.JButton
 
 
